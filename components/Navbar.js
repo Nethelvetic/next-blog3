40d274aa19a6d86e65cdc42163eb907fbc71200b
Golliard <golliard.info@gmail.com>
2024-11-08 21:00:12 +0100

    Fix Link component usage in Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,27 @@
+import Link from 'next/link';
+
 export default function Navbar() {
-    return (
-      <nav className="bg-blue-600 p-4">
-        <div className="container mx-auto flex justify-between items-center">
-          <a href="/" className="text-white font-bold text-xl">Mon Blog</a>
-          <div className="space-x-4">
-            <a href="/mesBlogs" className="text-white">Mes Blogs</a>
-            <a href="/about" className="text-white">À propos</a>
-            <a href="/contact" className="text-white">Contact</a>
-          </div>
+  return (
+    <nav className="bg-blue-600 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/" className="text-white font-bold text-xl">
+          Mon Blog
+        </Link>
+        <div className="space-x-4">
+          <Link href="/" className="text-white">
+            Accueil
+          </Link>
+          <Link href="/mesBlogs" className="text-white">
+            Mes Blogs
+          </Link>
+          <Link href="/about" className="text-white">
+            À propos
+          </Link>
+          <Link href="/contact" className="text-white">
+            Contact
+          </Link>
         </div>
-      </nav>
-    );
-  }
-  
+      </div>
+    </nav>
+  );
+}
